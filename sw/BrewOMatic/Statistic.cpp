41a538d4f3011782ac1c2cc63444b96cd07d61e5
Clément Léger <clement@clement-leger.fr>
2018-07-24 11:37:42 +0200

    Add sampling time in start of statistic output

--- sw/BrewOMatic/Statistic.cpp
+++ sw/BrewOMatic/Statistic.cpp
@@ -11,6 +11,22 @@
 static SdBaseFile *logFile;
 #endif
 
+#define MAX_BUF_SIZE	10
+
+static char buf[MAX_BUF_SIZE];
+
+static void statFlushBuffer()
+{
+#if ENABLED(STAT_OUTPUT_SD) && ENABLED(USE_SD_CARD)
+	int size = strlen(buf);
+	logFile->write(buf, size);
+	logFile->sync();
+#endif
+#if ENABLED(STAT_OUTPUT_SERIAL)
+	outSerial.print(buf);
+#endif
+}
+
 char statStart()
 {
 	char ret = 0;
@@ -19,12 +35,12 @@ char statStart()
 	if (logFile == NULL)
 		ret = 1;
 #endif
+	memset(buf, 0, MAX_BUF_SIZE);
+	/* Add sampling interval */
+	strcpy(buf, "#i:" STRINGIFY(STAT_OUTPUT_INTERVAL));
+
 	return ret;
 }
-
-#define MAX_BUF_SIZE	10
-
-static char buf[MAX_BUF_SIZE];
 
 void statOutputTemp(unsigned char temp, unsigned char targetTemp)
 {
@@ -34,14 +50,7 @@ void statOutputTemp(unsigned char temp, unsigned char targetTemp)
 	buf[strlen(buf)] = ':';
 	itoa(targetTemp, &buf[strlen(buf)], 10);
 	buf[strlen(buf)] = '\n';
-	int size = strlen(buf);
-#if ENABLED(STAT_OUTPUT_SD) && ENABLED(USE_SD_CARD)
-	logFile->write(buf, size);
-	logFile->sync();
-#endif
-#if ENABLED(STAT_OUTPUT_SERIAL)
-	outSerial.print(buf);
-#endif
+	statFlushBuffer();
 }
 
 void statStop()
